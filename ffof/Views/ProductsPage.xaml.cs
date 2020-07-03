ba3424e83c51d830e3e5acbd67872647c8d32e34
linhdt00474 <ddangtuanlinh@gmail.com>
2020-07-03 15:23:52 +0700

    Update ProductsPage.xaml.cs

--- ffof/Views/ProductsPage.xaml.cs
+++ ffof/Views/ProductsPage.xaml.cs
@@ -71,7 +71,6 @@ namespace ffof.Views
             }
             BrandLogo.IsVisible = false;
             var baseline = e.VerticalOffset - 152;
-            var delta = Math.Max(152 - baseline, -(Device.RuntimePlatform == Device.iOS ? 32 : 24));
             var delta = Math.Max(152 - baseline, -(Device.RuntimePlatform == Device.iOS ? 32 : 0));
 
             container.Margin = new Thickness(0, delta, 0, Math.Min(-delta, 0));
